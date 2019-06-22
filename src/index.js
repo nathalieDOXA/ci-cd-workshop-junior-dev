7c073f66c33eb74fc7b1854460924f58153188fa
support@doxa-holdings.com <support@doxa-holdings.com>
2019-06-22 13:04:49 +0800

    add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const feature1 = require('./feature1').default;
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(3000, () =>
     console.log('Example app listening on port 3000!')
